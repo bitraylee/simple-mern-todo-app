99f3909b898b093f0fcea960ac7525e3428f51f7
bitraylee <anshumohanta@hotmail.com>
2021-10-31 17:37:55 +0530

    initial commit

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+   try {
+      const connectionParams = {
+         useNewUrlParser: true,
+         useCreateIndex: true,
+         useUnifiedTopology: true,
+      };
+
+      await mongoose.connect(
+         "mongodb://localhost/simple-mern-todo-app",
+         connectionParams
+      );
+      console.log("Connected to Database");
+   } catch (error) {
+      console.log("Could not connect to database.", error);
+   }
+};
